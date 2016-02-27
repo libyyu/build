b93d076447e536e9ca7fc7ef14074dab12cceaee
libyyu <389465209@qq.com>
2016-02-27 11:24:12 +0800

    remove unuse files

--- build-win32.bat
+++
@@ -1,6 +0,0 @@
-set path=%path%;E:/WorkSpace/Game/MinGW/MinGW/x86/bin
-
-@set ENV86="E:/WorkSpace/Game/MinGW/MinGW/x86/msys/1.0/msys.bat"
-
-call "%ENV86%"
-
--- build-win64.bat
+++
@@ -1,5 +0,0 @@
-set path=%path%;E:/WorkSpace/Game/MinGW/MinGW/x64/bin
-
-@set ENV64="E:/WorkSpace/Game/MinGW/MinGW/x64/msys/1.0/msys.bat"
-
-call "%ENV64%"
