4ad800e2623ab7a9f87bf6572b26e7a670f199b4
libyyu <389465209@qq.com>
2016-04-16 22:59:14 +0800

    native lua

---
+++ lua-5.1.5/src/lua.hpp
@@ -0,0 +1,20 @@
+// C++ wrapper for LuaJIT header files.
+#ifdef   __cplusplus
+extern "C" {
+#endif
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+#ifdef __cplusplus
+}
+#endif
+
+
+#ifdef   __cplusplus
+    #define  __CFunBegin   extern "C" {
+    #define  __CFunEnd     }
+#else
+	#define  __CFunBegin
+	#define  __CFunEnd
+#endif//__cplusplus
+
