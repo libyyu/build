57314085c9ceaa44ec0cf50fbafb5a4db6d8db47
libyyu <389465209@qq.com>
2016-04-18 22:56:51 +0800

    engine

--- Project-Source/Win32DllMain.cpp
+++ Project-Source/Win32DllMain.cpp
@@ -6,10 +6,10 @@
 
 #include <Windows.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+	)
 {
 	switch (ul_reason_for_call)
 	{
@@ -26,7 +26,7 @@ _FCFunBegin
 LUALIB_API void luaL_openlibs(lua_State *L);
 LUA_API void luaS_openextlibs(lua_State *L);
 
-LUAOPEN_MODULE(MODULE_NAME)
+LUAOPEN_MODULE(FengEngine)
 {
 	luaL_openlibs(L);
 	luaS_openextlibs(L);
